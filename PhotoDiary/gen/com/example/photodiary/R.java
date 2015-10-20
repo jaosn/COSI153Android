1fadcd9f1c125011bfbe961d75ff3727e478b586
jaosn <hanzhenyugg@gmail.com>
2015-10-20 17:46:10 -0400

    main UI done sample

--- PhotoDiary/gen/com/example/photodiary/R.java
+++ PhotoDiary/gen/com/example/photodiary/R.java
@@ -23,15 +23,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pd_sample=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int botm_lg_login=0x7f08000b;
         public static final int botm_login=0x7f080000;
         public static final int botm_registar=0x7f080001;
         public static final int editText_password=0x7f080007;
         public static final int editText_re_password=0x7f08000a;
         public static final int editText_username=0x7f080004;
+        public static final int id_fragment_content=0x7f08000c;
         public static final int linearLayout_login=0x7f080002;
         public static final int linearLayout_pswd=0x7f080005;
         public static final int linearLayout_re_pswd=0x7f080008;
@@ -42,6 +44,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int login=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
