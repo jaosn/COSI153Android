e409f112e5eb455983b9f8334b15ac50078afa90
jaosn <hanzhenyugg@gmail.com>
2015-10-20 16:43:34 -0400

    login UI and StartUp UI done

--- PhotoDiary/gen/com/example/photodiary/R.java
+++ PhotoDiary/gen/com/example/photodiary/R.java
@@ -25,9 +25,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f08000c;
+        public static final int botm_lg_login=0x7f08000b;
         public static final int botm_login=0x7f080000;
         public static final int botm_registar=0x7f080001;
+        public static final int editText_password=0x7f080007;
+        public static final int editText_re_password=0x7f08000a;
+        public static final int editText_username=0x7f080004;
+        public static final int linearLayout_login=0x7f080002;
+        public static final int linearLayout_pswd=0x7f080005;
+        public static final int linearLayout_re_pswd=0x7f080008;
+        public static final int textView_login=0x7f080003;
+        public static final int textView_password=0x7f080006;
+        public static final int textView_re_pswd=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -41,7 +51,14 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int botm_login=0x7f050003;
         public static final int botm_registar=0x7f050004;
+        public static final int confirm=0x7f050007;
+        public static final int confirm_samp=0x7f05000b;
         public static final int hello_world=0x7f050001;
+        public static final int lg_pswd=0x7f050006;
+        public static final int lg_pswd_samp=0x7f05000a;
+        public static final int lg_usrn=0x7f050005;
+        public static final int lg_usrn_samp=0x7f050009;
+        public static final int login=0x7f050008;
     }
     public static final class style {
         /** 
